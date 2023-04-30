d994e4bf52c8568f0a908f1c80e4952833744c68
whddbsl <mm_jong@naver.com>
2023-04-30 17:49:49 +0900

    only js

---
+++ main.js
@@ -0,0 +1,70 @@
+const Authorization =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6Ijg4OTc5Njk4NiIsImF1dGhfaWQiOiIyIiwiZXhwIjoxNjk4Mzg5NjEzLCJpYXQiOjE2ODI4Mzc2MTMsIm5iZiI6MTY4MjgzNzYxMywic2VydmljZV9pZCI6IjQzMDAxMTQ4MSIsInRva2VuX3R5cGUiOiJBY2Nlc3NUb2tlbiJ9.ScIXRE8j8IwHFScO-2uNbBf8cK7dnFiVxxi_dKX6CJk";
+let inputNickName = "";
+let searchButton = document.getElementById("search-button");
+let userInput = document.getElementById("user-input"); // 유저가 입력한 닉네임 가져오기
+let matchButton = document.getElementById("match-button")
+let resultButton = document.getElementById("result-button")
+
+let data = null; // 전역 변수로 선언
+let accessID = null; //
+function getUserId() {// 유저 고유 식별자 가져오기
+  inputNickName = userInput.value;
+  async function getData() {
+    let header = new Headers({
+      Authorization: Authorization,
+    });
+    const url = `https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname=${inputNickName}`;
+    const response = await fetch(url, { headers: header });
+    return await response.json(); // Promise를 반환하도록 변경
+  }
+  getData()
+    .then((result) => {
+      data = result; // 전역 변수에 결과 저장
+      accessID = data.accessId;
+      console.log(data);
+      console.log(accessID);
+      renderUserInfo();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+searchButton.addEventListener("click", getUserId);
+
+function renderUserInfo() {
+  userHTML = ``;
+  userHTML += `닉네임 : ${data.nickname}<br>
+    레벨 : ${data.level}`;
+  document.getElementById("user-board").innerHTML = userHTML;
+}
+
+let matchData = []
+async function getUserMatch(){//유저 친선경기 매치 목록 불러오기
+    let header = new Headers({
+        Authorization: Authorization,
+    })
+    const url = `https://api.nexon.co.kr/fifaonline4/v1.0/users/${accessID}/matches?matchtype=40&offset=0&limit=20`
+    const response = await fetch(url, {headers: header});
+    matchData = await response.json();
+    console.log(matchData)
+}
+
+
+async function getMatchDetail(){
+    let header = new Headers({
+        Authorization: Authorization,
+    })
+    for(i=0;i<matchData.length;i++){
+        console.log(matchData[i])
+        const url =`https://api.nexon.co.kr/fifaonline4/v1.0/matches/${matchData[i]}`
+        const response = await fetch(url, {headers: header});
+        let data = await response.json()
+        console.log("경기결과",data.matchInfo[0].matchDetail.matchResult, "상대",data.matchInfo[1].nickname)
+    }    
+}
+
+resultButton.addEventListener("click",getMatchDetail)
+matchButton.addEventListener("click",getUserMatch)
+
